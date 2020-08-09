e3ec2e6377763c4b62af8d36848b2159448a17d4
jasbel <asbel.elmec@gmail.com>
2020-08-09 13:31:27 -0400

    [ADD] UPDATE RESPONSIVE SASS

--- gulpfile.js
+++ gulpfile.js
@@ -50,7 +50,7 @@ const sassOptions = {
 
 // File paths
 const files = {
-    scssPath: 'src/scss/**/*.scss',
+    scssPath: 'src/scss/**/*',
     stylePath: 'src/scss/styles.scss',
     jsPath: 'src/js/*.js',
     htmlPath: 'src/**/*.pug',
@@ -71,7 +71,6 @@ function scssTask() {
         .pipe(postcss([autoprefixer(), cssnano()])) // PostCSS plugins
         .pipe(sourcemaps.write('.')) // write sourcemaps file in current directory
         .pipe(dest('dist/css')) // put final CSS in dist folder
-        .pipe(browserSync.reload({ stream: true })); //recharge page
 }
 
 // JS task: concatenates and uglifies JS files to script.js
@@ -83,7 +82,6 @@ function jsTask() {
         .pipe(concat('main.min.js'))
         .pipe(uglify())
         .pipe(dest('dist/js'))
-        .pipe(browserSync.reload({ stream: true }));
 }
 
 //Minify images
@@ -92,14 +90,12 @@ function imagesTask() {
         .pipe(newer("dist/images"))
         .pipe(imagemin())
         .pipe(dest("dist/images"))
-        .pipe(browserSync.reload({ stream: true }));
 }
 
 // Copy fonts
 function fontTask() {
     return src([files.fontPath])
         .pipe(dest("dist/fonts"))
-        .pipe(browserSync.reload({ stream: true }));
 }
 
 //Pug Preprocesador HTML (only index)
@@ -107,7 +103,6 @@ function pugHTMLTask() {
     return src([files.htmlPath])
         .pipe(pug())
         .pipe(dest("dist/"))
-        .pipe(browserSync.reload({ stream: true }));
 }
 
 //js Babel codigo moderno
@@ -115,13 +110,11 @@ function jsBabelTask() {
     return src([files.jsPath])
         .pipe(minifyJS({ mangle: { keepClassName: true } }))
         .pipe(dest("dist/js"))
-        .pipe(browserSync.reload({ stream: true }))
 }
 //index HTML
 function indexHtmlTask() {
     return src([files.indexHtmlPath])
         .pipe(dest("dist/"))
-        .pipe(browserSync.reload({ stream: true }))
 }
 
 //FILES FOR GITHUB-PAGES MASTER/DOCS
@@ -136,7 +129,13 @@ function docsGitHUb() {
 // Watch task: watch SCSS and JS files for changes
 // If any change, run scss and js tasks simultaneously
 function watchTask() {
-    browserSync.init({
+    var filess = [
+        '**/*.html',
+        'src/css/**/*.css',
+        'src/js/**/*.js',
+        'src/scss/**/*.scss'
+     ];
+    browserSync.init(filess, {
         server: {
             baseDir: "dist",
             index: "index.html"
@@ -144,21 +143,19 @@ function watchTask() {
         notify: false,
         injectChanges: true
     });
-    watch([files.scssPath, files.jsPath], { interval: 1000, usePolling: true }, //Makes docker work
+   /*  watch([files.scssPath, files.jsPath], { interval: 1000, usePolling: true }, //Makes docker work
         series(
             parallel(scssTask, jsTask)
             // cacheBustTask
         )
-    );
-    // watch([files.scssPath], series(scssTask));
-    // watch([files.jsPath], series(jsTask));
+    ); */
+    watch([files.scssPath], series(scssTask))
+    watch([files.jsPath], series(jsTask));
     watch([files.imagesPath], series(imagesTask));
     watch([files.fontPath], series(fontTask));
     watch([files.indexHtmlPath], series(indexHtmlTask));
     // watch([files.htmlPath], series(pugHTMLTask));
-    // watch([files.jsPath], series(jsBabelTask));
-    watch("dist/*").on("change", browserSync.reload);;
-    watch("src/*.html").on("change", browserSync.reload);;
+    // watch([files.jsPath], series(jsBabelTask));;
 }
 
 /*********************** */
